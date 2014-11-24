247097d677d41a995924b21f070b91cee6efc70d
Josh Mu <mu@joshmu.com>
2014-11-24 19:47:08 +1100

    add shop.html with dynamic themes

--- app/dist/js/bundle.js
+++ app/dist/js/bundle.js
@@ -160,15 +160,21 @@ angular.module('starterTemplate')
         var p = {};
 
         var digitalPosters = {
-            title: 'Digital Posters'
+            title: 'Digital Posters',
+            description: 'Looking for a new way to open up your event program? Want to offer something interactive to your delegates? Ready to break new ground in program formatting? Take something old and make it new again and you have Digitalposter.com.au.',
+            theme: 'deep-purple'
         };
 
         var eventranet = {
-            title: 'Eventranet'
+            title: 'Eventranet',
+            description: 'As the name suggests, Eventranet is designed to bring all related technology driven components of an event together to give users one direct point of access. Not unlike an organisations internal intranet, this product is an events intranet and onsite interactive service for delegates to access all their event digital needs.',
+            theme: 'blue'
         };
 
         var speakersPrep = {
-            title: 'Speakers Prep'
+            title: 'Speakers Prep',
+            description: 'Speakersprep.com, hosted and run by Lucien Mark, is suited to speaker and presentation management of an event. Its purpose is to remove the manual process of presentation upload and editing on site.',
+            theme: 'green'
         };
 
         var products = [digitalPosters, eventranet, speakersPrep];
